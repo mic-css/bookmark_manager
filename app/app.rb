d7932c77775b8477b3418b1f10053ad1376b8c36
itsindigo <matthewbridgesbusiness@gmail.com>
2016-02-09 15:51:40 +0000

    moved files into app directory, created links view

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require './app/models/link.rb'
 
 class BookmarkManager < Sinatra::Base
+  # enable :sessions
+
   get '/' do
     'Hello BookmarkManager!'
+  end
+
+  get '/links' do
+    erb :links
   end
 
   # start the server if ruby file executed directly
