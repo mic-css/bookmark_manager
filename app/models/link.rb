d7f108a7493ed9bdcd72a9fe1e3883fb2fce5283
Mic <mic.cassano@gmail.com>
2016-02-10 11:21:32 +0000

    Set up Heroku

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,6 @@ class Link
   property :added_at,   DateTime
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
