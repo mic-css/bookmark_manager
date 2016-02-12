936c13a2709f4ce9609df29231c31463ce3588aa
Mic <mic.cassano@gmail.com>
2016-02-12 10:22:09 +0000

    Refactor

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,12 +1,9 @@
 require 'link'
 
-
 describe Link do
-
   subject{described_class.create(title: 'Makers Academy', url: 'http://www.makersacademy.com/')}
 
   it{is_expected.to respond_to(:title)}
   it{is_expected.to respond_to(:url)}
   it{is_expected.to respond_to(:id)}
-
 end
